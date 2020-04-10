b5788018fc5c4d80c5f62d9283fc9e29e09dd086
splouk <david@faire.com>
2020-04-10 13:46:53 -0400

    initialize firebase

--- src/Firebase.tsx
+++ src/Firebase.tsx
@@ -2,14 +2,24 @@ import { singletonGetter } from "./singletonGetter";
 import * as firebase from "firebase";
 import { observable } from "mobx";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBJGCUEZiQnLggmg2hKYMFU5TRj_26FDgc",
+  authDomain: "meal-schedule-45307.firebaseapp.com",
+  databaseURL: "https://meal-schedule-45307.firebaseio.com",
+  projectId: "meal-schedule-45307",
+  storageBucket: "meal-schedule-45307.appspot.com",
+  messagingSenderId: "808438742383",
+  appId: "1:808438742383:web:cbf448b9608199321d3c9a",
+};
+
 export class Firebase {
   get = singletonGetter(Firebase);
 
   @observable
-  firestore = undefined;
+  firestore: firebase.firestore.Firestore | undefined = undefined;
 
   constructor() {
-    firebase.initializeApp({});
+    firebase.initializeApp(firebaseConfig);
     this.firestore = firebase.firestore();
   }
 }
